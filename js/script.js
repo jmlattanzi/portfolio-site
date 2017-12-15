86bef63d411bd9976df4ee5c94b8febfc3178bde
jmlattanzi <jonathanlattanzi@gmail.com>
2017-12-15 17:17:20 -0600

    Added button function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.hidden').hide()
+  $('#about').click(function() {
+    $('#about-page').toggle()
+  })
+  $('#projects').click(function() {
+    $('#projects-page').toggle()
+  })
+  $('#contact').click(function() {
+    $('#contact-page').toggle()
+  })
+  $('#social').click(function() {
+    $('#social-page').toggle()
+  })
+})
